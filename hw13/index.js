de90b9bde1698fa16d7bfd76b8d19ad5a4c30479
Kostia1001 <strong.ogurcov@gmail.com>
2022-11-01 22:32:04 +0300

    fix sort

--- hw13/index.js
+++ hw13/index.js
@@ -1,6 +1,6 @@
 const arr = ["HTML", "JavaScript", "CSS"];
 
-const copySorted = arrInput => arrInput.slice()
+const copySorted = arrInput => arrInput.slice().sort()
  
 const sorted = copySorted(arr)
 alert (sorted); 
